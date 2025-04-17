38058dee6ed51a923b27cf9026c5cb1bc8a5026b
aohuuhneyugn <112865451+aohuuhneyugn@users.noreply.github.com>
2025-04-17 13:39:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask CI/CD with Jenkins!"
+    return "Hello, CI/CD with Jenkins!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+    app.run(host="0.0.0.0", port=5000, debug=True)
